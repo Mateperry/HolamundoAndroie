6970a856d7d78f087bf1e9709232f81444c5eae5
Mateo <castromateo2004@gmail.com>
2025-03-03 22:14:13 -0500

    Subiendo todos los archivos del proyecto de fútbol

--- app/src/main/java/com/mateocastro/holamundo/MainActivity.kt
+++ app/src/main/java/com/mateocastro/holamundo/MainActivity.kt
@@ -1,20 +1,52 @@
 package com.mateocastro.holamundo
 
+
+
+
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val num1 = findViewById<EditText>(R.id.editTextNumber1)
+        val num2 = findViewById<EditText>(R.id.editTextNumber2)
+        val resultado = findViewById<TextView>(R.id.textViewResultado)
+
+        val btnSuma = findViewById<Button>(R.id.btnSuma)
+        val btnResta = findViewById<Button>(R.id.btnResta)
+        val btnMultiplicacion = findViewById<Button>(R.id.btnMultiplicacion)
+        val btnDivision = findViewById<Button>(R.id.btnDivision)
+
+        btnSuma.setOnClickListener {
+            val res = num1.text.toString().toDouble() + num2.text.toString().toDouble()
+            resultado.text = "Resultado: $res"
+        }
+
+        btnResta.setOnClickListener {
+            val res = num1.text.toString().toDouble() - num2.text.toString().toDouble()
+            resultado.text = "Resultado: $res"
+        }
+
+        btnMultiplicacion.setOnClickListener {
+            val res = num1.text.toString().toDouble() * num2.text.toString().toDouble()
+            resultado.text = "Resultado: $res"
+        }
+
+        btnDivision.setOnClickListener {
+            val num2Value = num2.text.toString().toDouble()
+            if (num2Value != 0.0) {
+                val res = num1.text.toString().toDouble() / num2Value
+                resultado.text = "Resultado: $res"
+            } else {
+                resultado.text = "Error: División por cero"
+            }
         }
     }
 }
+
